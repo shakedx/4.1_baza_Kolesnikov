9babe6592f74cee6ecb75658a375ef5515283bcb
shakedx <kolesnikov_danil123@mail.ru>
2021-11-11 12:20:35 +0200

    Добавьте файлы проекта.

---
+++ 4.1_baza_Kolesnikov/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _4._1_baza_Kolesnikov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] M = new double[15];
+            Random rnd = new Random();
+            for (int i = M.Length-1; i >=0 ;i--)
+            {
+                M[i] = Math.Round(rnd.NextDouble() * 100, 2);
+                Console.WriteLine(M[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
